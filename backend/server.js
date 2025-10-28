cf5642474193708f657ce21523565e06ea452dfb
AnshulVishwa <anshulvishwa2205@gmail.com>
2025-10-28 05:56:27 +0530

    Adding Frontend url

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const cors = require("cors")
 const app = express();
 app.use(helmet());
 app.use(express.json());
-app.use(cors())
+app.use(cors({origin:"https://secure-file-x-indol.vercel.app/"}))
 
 const upload = multer({ dest: "uploads/" });
 
